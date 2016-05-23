e0a7fad60c032805bb1ebda5d35a3f41fb266891
Maxim <drenyaev.max@gmail.com>
2016-05-23 13:57:49 +0300

    h

--- Lab/menu.h
+++
@@ -1,5 +0,0 @@
-void menuInput(void); //меню добавления
-void menuSearchPass(void); //меню поиска пассажира
-void menuSearchVoyage(void); //меню поиска рейса
-void MainMenu(void); //главное меню
-int mainMenu(char *file1, char *file2);//функция главное меню  
